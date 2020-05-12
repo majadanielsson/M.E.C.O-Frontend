fcbe80999a4ad9e65f013db9bf0fc45a5216a901
christianwiren <christian@wiren.cc>
2020-05-12 15:13:57 +0200

    working

--- src/views/Csv.vue
+++ src/views/Csv.vue
@@ -2,9 +2,13 @@
   <div>
     <navbar name="Kursvärderingsportalen" url="/csv"></navbar>
     <b-container class="my-4">
-      <h1 class="text-dark">Ladda upp formulär</h1>
-      <b-row>
+      <b-row align-h="center">
         <b-col md="10" lg="8">
+          <h1 class="text-dark">Uppladdning av kursvärderingar</h1>
+          <p>
+            <b-link href="/download/Kursvärdering.csv" download>Ladda ner mallen</b-link>, fyll i, och ladda upp
+          </p>
+
           <b-form @submit.prevent="$api.csv(file)">
             <b-form-file
               placeholder="Välj eller släpp en fil..."
